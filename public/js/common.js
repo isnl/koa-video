4a5d8aad00545bb0f83061ea5c40d2c522759d88
peanut <htmlcs@163.com>
2020-11-08 00:08:14 +0800

    feat: 分割template

--- public/js/common.js
+++ public/js/common.js
@@ -1,88 +1,8 @@
-$(function () {
-  const $btn = $(".play-list-box").children("button");
-  const [name, source] = $btn.eq(0).attr("data-src").split("$");
-  //默认激活第一个按钮
-  $btn.eq(0).addClass("active");
-  new Aliplayer(
-    {
-      id: "video-play",
-      source,
-      width: "100%",
-      height: "460px",
-      autoplay: true,
-      isLive: false,
-      rePlay: false,
-      playsinline: true,
-      preload: true,
-      controlBarVisibility: "click",
-      useH5Prism: true
-    },
-    function (player) {
-      initEvent(player);
-    }
-  );
+//获取邮箱
+$(".get-email").click(() => {
+  layer.msg("htmlcs@163.com");
 });
-function initEvent(player) {
-  //切换播放
-  $(".play-list-box")
-    .children("button")
-    .on("click", function () {
-      const [name, src] = $(this).attr("data-src").split("$");
-      player.loadByUrl(src);
-      $(this).siblings().removeClass("active");
-      $(this).addClass("active");
-    });
-
-  //复制下载链接
-  $(".copy-one-link").on("click", function () {
-    const [name, src] = $(this).prev().text().split("$");
-    copyText(src, () => {
-      layer.msg("复制成功");
-    });
-  });
-  //复制名称+下载链接
-  $(".copy-one-all").on("click", function () {
-    const text = $(this).prev().prev().text();
-    copyText(text, () => {
-      layer.msg("复制成功");
-    });
-  });
-
-  //复制所有名称+链接
-  $(".copy-all").click(function () {
-    let str = ``;
-    $(".download-list li").each((index, item) => {
-      const [name, src] = $(item).children(".text").text().split("$");
-      index === $(".download-list li").length - 1
-        ? (str += `${name}$${src}`)
-        : (str += `${name}$${src}\n`);
-    });
-    str += `\n---哎呦影视采集:https://v.iiter.cn`;
-    copyText(str, () => {
-      layer.msg("复制成功");
-    });
-  });
-  //复制所有链接
-  $(".copy-all-link").click(function () {
-    let str = ``;
-    $(".download-list li").each((index, item) => {
-      const [name, src] = $(item).children(".text").text().split("$");
-      index === $(".download-list li").length - 1
-        ? (str += `${src}`)
-        : (str += `${src}\n`);
-    });
-    str += `\n---哎呦影视采集:https://v.iiter.cn`;
-    copyText(str, () => {
-      layer.msg("复制成功");
-    });
-  });
-  //获取邮箱
-  $(".get-email").click(() => {
-    layer.msg("htmlcs@163.com");
-  });
-}
-
-function copyText(text, callback) {
+var copyText = function (text, callback) {
   // text: 要复制的内容， callback: 回调
   let tag = document.createElement("textarea");
   tag.setAttribute("id", "cp_hgz_input");
@@ -94,4 +14,4 @@ function copyText(text, callback) {
   if (callback) {
     callback(text);
   }
-}
+};
