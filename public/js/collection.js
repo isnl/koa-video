7d2de2fce72c3cb1830a7373bba4b2cd363fc172
peanut <htmlcs@163.com>
2020-11-12 12:01:55 +0800

    feat: release v1.0

--- public/js/collection.js
+++ public/js/collection.js
@@ -1,8 +1,14 @@
 $(".collection-btn").click(function () {
   const m = $(this).attr("data-src");
-  $.get(`/collection?o=zuidazy&m=${m}`).then(res => {
-    if (res.status) {
-      location.href = `/zuidazy/${m}`;
-    }
-  });
+  $.get(`/collection?o=zuidazy&m=${m}`)
+    .then(res => {
+      if (res.status) {
+        layer.msg("采集成功，即将跳转至播放页", function () {
+          location.href = `/zuidazy/${m}`;
+        });
+      }
+    })
+    .catch(err => {
+      layer.msg("采集失败，请刷新后重试");
+    });
 });
